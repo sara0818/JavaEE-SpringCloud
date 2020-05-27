56b40658904844a836fc9de689a9328f5e959469
sara <1179510996@qq.com>
2020-05-28 02:04:33 +0800

    创建springcloud netflix zuul

---
+++ springcloud-zuul/src/main/java/com/wxr/springcloudzuul/SpringcloudZuulApplication.java
@@ -0,0 +1,17 @@
+package com.wxr.springcloudzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringcloudZuulApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringcloudZuulApplication.class, args);
+  }
+
+}
